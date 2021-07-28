d3df293393f5315c088b17f4b77e029fc106597c
Dmitry Modin <xzwiex@lembrand.com>
2021-07-28 15:11:34 +0300

    Fix example

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { CurrencyConverter } from './converter';
 
 const converter = new CurrencyConverter();
 converter.setExchangeRate('EUR', 'RUB', 88);
-// converter.setExchangeRate('USD', 'EUR', 1.2);
+// converter.setExchangeRate('EUR', 'USD', 1.2);
 converter.setExchangeRate('EUR', 'TRY', 10.1);
 converter.setExchangeRate('USD', 'TRY', 8.56);
 console.log('convert', converter.getExchangeRate('USD', 'RUB'));
